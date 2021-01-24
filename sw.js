374dc2f452004a3b5505a096216bd22535558d10
NickLiffen <nickliffen@gmail.com>
2021-01-24 17:59:44 +0000

    Getting Service Worker Ready

--- sw.js
+++ sw.js
@@ -1,48 +1,26 @@
-var cacheName = 'NickLiffenBlog';
-
-var appShellFiles = [
-  '/',
-  '/404.html',
-  '/index.html',
-  '/css/404.html',
-  '/css/hyde.html',
-  '/css/poole.html'
-];
-
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(appShellFiles);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './404.html',
+        './css/404.css',
+        './css/hyde.css',
+        './css/poole.css'
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        return caches.open('v1').then((cache) => {
+          cache.put(event.request, response.clone());
           return response;
         });
       });
-    })
-  );
-});
-
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-        if(key !== cacheName) {
-          return caches.delete(key);
-        }
-      }));
     })
   );
 });
