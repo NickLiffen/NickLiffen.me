825d03fca53203fa28203280c4d9daec5c4788d1
NickLiffen <nickliffen@gmail.com>
2021-01-24 17:46:48 +0000

    Service Worker

--- sw.js
+++ sw.js
@@ -1,31 +1,47 @@
-const version = "1.0.0";
-const cacheName = `nickliffenblog-${version}`;
-self.addEventListener('install', e => {
+var cacheName = 'NickLiffenBlog';
+
+var appShellFiles = [
+  '/404.html',
+  '/index.html',
+  '/css/404.html',
+  '/css/hyde.html',
+  '/css/poole.html'
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        `/`,
-        `/index.html`,
-        `/404.html`,
-        `/css/main.css`,
-        `/css/hyde.css`,
-        `/css/poole.css`,
-      ])
-        .then(() => self.skipWaiting());
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+        if(key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
