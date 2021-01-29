3a91952e96f876d06850b5c132b530be396e76e0
NickLiffen <nickliffen@gmail.com>
2021-01-29 21:11:16 +0000

    New Service Worker

--- sw.js
+++ sw.js
@@ -1,46 +1,41 @@
 const version = "v1.0.3"; // <--- Change this on every release
-const cache = `nickliffenblog-${version}`;
-const files = [
-  './index.html',
-  './404.html',
-  './css/hyde.css',
-  './css/poole.css',
-  './articles/increasing-adoption-of-ghas-code-scanning-at-scale.html',
-  './articles/building-a-simple-website.html'
-]
+const cacheName = `nickliffenblog-${version}`;
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cache).then((cache) => {
-      return cache.addAll(files);
-    }).catch((err) => {
-      console.log('Error Cache Open (Install)', err)
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/404.html',
+        '/css/hyde.css',
+        '/css/poole.css',
+        '/articles/increasing-adoption-of-ghas-code-scanning-at-scale.html',
+        '/articles/building-a-simple-website.html'
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request).then((response) => {
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
         let responseClone = response.clone();
-        caches.open(cache).then((c) => {
-          c.put(event.request, responseClone);
-        }).catch((err) => {
-          console.log('Error Cache Open (Fetch)', err)
-        })
+        caches.open(cacheName).then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
         return response;
-      }).catch((err) => {
-        console.log('Error Fetch (Fetch)', err)
-      })
-    }).catch((err) => {
-      console.log('Error Cache Match (Fetch)', err)
-    })
-  );
+      }).catch(function () {
+        return caches.match('/404.html');
+      });
+    }
+  }));
 });
 
 self.addEventListener('activate', (event) => {
-  var cacheKeeplist = [cache];
+  var cacheKeeplist = [cacheName];
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
