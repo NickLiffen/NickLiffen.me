db6743a2a7117c7b2598f471925eef1eff9adbc0
NickLiffen <nickliffen@gmail.com>
2021-01-29 22:12:08 +0000

    Cleaning up HTML 404 File

--- sw.js
+++ sw.js
@@ -1,49 +1,64 @@
-const PRECACHE = 'precache-v2';
-const RUNTIME = 'runtime';
+var VERSION = '1';
 
-const PRECACHE_URLS = [
-  '/index.html',
-  '/404.html',
-  '/css/hyde.css',
-  '/css/poole.css',
-  '/articles/increasing-adoption-of-ghas-code-scanning-at-scale.html',
-  '/articles/building-a-simple-website.html'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
+this.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(VERSION).then(cache => {
+    return cache.addAll([
+      '/',
+      '/sw.js',
+      '/index.html',
+      '/404.html',
+      '/css/hyde.css',
+      '/css/poole.css',
+      '/articles/increasing-adoption-of-ghas-code-scanning-at-scale.html',
+      '/articles/building-a-simple-website.html'
+    ]);
+  }))
 });
 
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
+this.addEventListener('fetch', function(e) {
+  var tryInCachesFirst = caches.open(VERSION).then(cache => {
+    return cache.match(e.request).then(response => {
+      if (!response) {
+        return handleNoCacheMatch(e);
+      }
+      // Update cache record in the background
+      fetchFromNetworkAndCache(e);
+      // Reply with stale data
+      return response
+    });
+  });
+  e.respondWith(tryInCachesFirst);
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+this.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.map(key => {
+      if (key !== VERSION)
+        return caches.delete(key);
+    }));
+  }));
 });
+
+function fetchFromNetworkAndCache(e) {
+  // DevTools opening will trigger these o-i-c requests, which this SW can't handle.
+  // There's probaly more going on here, but I'd rather just ignore this problem. :)
+  // https://github.com/paulirish/caltrainschedule.io/issues/49
+  if (e.request.cache === 'only-if-cached' && e.request.mode !== 'same-origin') return;
+
+  return fetch(e.request).then(res => {
+    // foreign requests may be res.type === 'opaque' and missing a url
+    if (!res.url) return res;
+    // regardless, we don't want to cache other origin's assets
+    if (new URL(res.url).origin !== location.origin) return res;
+
+    return caches.open(VERSION).then(cache => {
+      // TODO: figure out if the content is new and therefore the page needs a reload.
+      cache.put(e.request, res.clone());
+      return res;
+    });
+  }).catch(err => console.error(e.request.url, err));
+}
+
+function handleNoCacheMatch(e) {
+  return fetchFromNetworkAndCache(e);
+}
